475eca94c4c630ec9307a0c9ee5523c7ad42181a
Jesujoba224 <jesujobaonabamiji54@gmail.com>
2025-11-25 09:13:44 +0100

    Add files via upload

---
+++ Print.py
@@ -0,0 +1,11 @@
+# Printing a greeting message
+print("Hello, world!")
+
+# Printing my name
+print("My Name is Jesujoba.")
+
+# Printing a fun fact about myself
+print("Ï love Coding and Eating.")
+
+# Perfoming and printing a simple calculation
+print("The sum of 5 and 3 is:", 5 + 3)
