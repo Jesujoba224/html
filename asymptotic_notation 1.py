43c096f76af8dda4d22d3181df1366b4d6670ec3
Jesujoba224 <jesujobaonabamiji54@gmail.com>
2026-02-07 11:20:35 +0000

    Add files via upload

---
+++ asymptotic_notation 1.py
@@ -0,0 +1,9 @@
+def printnumber(n):
+    iteration=0
+    print("The number entered by user is ",n)
+    iteration = iteration + 1
+    print("Total iterations done by the code is ",iteration,"\n")
+    
+printnumber(100000000000)
+printnumber(20)
+print("\n with any 'n' the time taken by our code won't change")
---
+++ asymptotic_notation 2.py
@@ -0,0 +1,11 @@
+def Ontime(n):
+    iteration=0
+    for i in range(1,n+1):
+        iteration = iteration + 1
+        print("When n is ",n," Iterations = ",iteration)
+        
+Ontime(100)
+Ontime(20)
+Ontime(42)
+
+print("\nWith every 'n' the time taken and iterations will increase")
